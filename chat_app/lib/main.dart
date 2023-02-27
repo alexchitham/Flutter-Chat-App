2d35dc35aa67337bd95540ab7d7d7f3e38e86386
alexchitham <alexchitham@hotmail.co.uk>
2023-02-27 11:40:21 +0000

    Fixed bug of white screen of launch
    Should now be fully connected to firebase

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -4,9 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
